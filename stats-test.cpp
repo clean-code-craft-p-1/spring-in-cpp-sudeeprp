a40775f269524cda3dd4dd319ee1d29052a90562
Sudeep <sudeep.rp@gmail.com>
2025-11-02 17:35:02 +0530

    by claude sonnet 4

--- stats-test.cpp
+++ stats-test.cpp
@@ -18,4 +18,7 @@ TEST(Statistics, AverageNaNForEmpty) {
     
     // Specify the EXPECT statement here.
     // Use http://www.cplusplus.com/reference/cmath/isnan/
+    EXPECT_TRUE(std::isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.max));
+    EXPECT_TRUE(std::isnan(computedStats.min));
 }
